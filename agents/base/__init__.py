6def407d5459c6c12d0931c9633746fe0e4cb9b6
Gustavo Santos <progustavosantos@gmail.com>
2025-08-09 09:14:02 -0300

    9/08 last updates

--- agents/base/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Módulo base para agentes - Classes compartilhadas
-"""
-
-from .logger import AgentLogger
-from .cloud_connector import CloudConnector
-
-__all__ = ['AgentLogger', 'CloudConnector']
-
--- agents/specialists/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Módulo de agentes especialistas - AWS e GCP
-"""
-
-from .aws_specialist import AWSSpecialistAgent
-from .gcp_specialist import GCPSpecialistAgent
-
-__all__ = ['AWSSpecialistAgent', 'GCPSpecialistAgent']
-
