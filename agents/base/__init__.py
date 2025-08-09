56adfc865b4b89e3f834e9a4f3382b416c604bff
Gustavo Santos <progustavosantos@gmail.com>
2025-08-09 06:11:34 -0300

    ai agents by crewai tool

---
+++ agents/base/__init__.py
@@ -0,0 +1,9 @@
+"""
+Módulo base para agentes - Classes compartilhadas
+"""
+
+from .logger import AgentLogger
+from .cloud_connector import CloudConnector
+
+__all__ = ['AgentLogger', 'CloudConnector']
+
